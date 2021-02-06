a7912ddc5f38049ef7ef646034d01e1bd44bc674
asaxena2019 <anushkasaxena2019@gmail.com>
2021-02-06 15:39:05 -0500

    connected current frontend to backend

--- server/routes/business-router.js
+++ server/routes/business-router.js
@@ -4,11 +4,6 @@ const BusinessCtrl = require('../controller/business-ctrl')
 
 const router = express.Router()
 
-router.post('/business', BusinessCtrl.createBusiness)
-router.put('/business/:id', BusinessCtrl.updateBusiness)
-router.delete('/business/:id', BusinessCtrl.deleteBusiness)
-router.get('/business/:id', BusinessCtrl.getBusinessById)
-router.get('/businesses', BusinessCtrl.getBusinesses)
-router.get('/businesses/get', BusinessCtrl.pipe)
+router.get('/', BusinessCtrl.pipe)
 
 module.exports = router
