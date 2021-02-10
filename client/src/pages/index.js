68cf664906e2f5ad7fa6b5ae3428b1a0d6771a4c
asaxena2019 <anushkasaxena2019@gmail.com>
2021-02-10 15:08:00 -0500

    removed front-end part

--- client/src/pages/index.js
+++
@@ -1,6 +0,0 @@
-import BusinessesList from './BusinessesList'
-import BusinessesInsert from './BusinessesInsert'
-import BusinessesUpdate from './BusinessesUpdate'
-import pipeBus from './BusinessGet'
-
-export { BusinessesList, BusinessesInsert, BusinessesUpdate, pipeBus }
