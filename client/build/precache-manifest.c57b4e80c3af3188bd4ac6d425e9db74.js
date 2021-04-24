ddcfc6cd2876637546e72497ed8ee11e8472ffd6
Laura <laura.j.yao@gmail.com>
2021-04-24 16:10:53 -0400

    yarn error fix

---
+++ client/build/precache-manifest.c57b4e80c3af3188bd4ac6d425e9db74.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2986b8dd3dd71985e087a09cefdb8e7d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b98c9d743d36af4bb93",
+    "url": "/static/css/main.833d8bda.chunk.css"
+  },
+  {
+    "revision": "7887f782a44f0280f1bd",
+    "url": "/static/js/2.e73005bc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e73005bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b98c9d743d36af4bb93",
+    "url": "/static/js/main.279425ef.chunk.js"
+  },
+  {
+    "revision": "943af287362f2ccad764",
+    "url": "/static/js/runtime-main.1a1dd5f1.js"
+  },
+  {
+    "revision": "3f85c38bb25ff6ebbbba4243c9941664",
+    "url": "/static/media/spotlight-photo.3f85c38b.jpg"
+  }
+]);
