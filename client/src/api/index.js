b8234bc256ef24fc9cdcaf523e7c66361f0b383a
asaxena2019 <anushkasaxena2019@gmail.com>
2021-04-25 23:45:18 -0400

    moves

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const host = 3000 || process.env.PORT;
+const host = process.env.PORT || 3000 ;
 
 const api = axios.create({
     baseURL: `http://localhost:${host}/api`
